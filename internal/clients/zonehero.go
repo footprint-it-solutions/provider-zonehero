f528ef8beff646b385aaf3c24cca778056010ed6
Alex Sunins <alex@Ian-Ms-Work-MacBook-Pro.local>
2025-09-04 14:08:46 +0100

    initial conversion to zonehero

--- internal/clients/zonehero.go
+++ internal/clients/zonehero.go
@@ -15,7 +15,7 @@ import (
 
 	"github.com/crossplane/upjet/pkg/terraform"
 
-	"github.com/upbound/upjet-provider-template/apis/v1beta1"
+	"github.com/footprint-it-solutions/provider-zonehero/apis/v1beta1"
 )
 
 const (
@@ -24,7 +24,9 @@ const (
 	errGetProviderConfig    = "cannot get referenced ProviderConfig"
 	errTrackUsage           = "cannot track ProviderConfig usage"
 	errExtractCredentials   = "cannot extract credentials"
-	errUnmarshalCredentials = "cannot unmarshal template credentials as JSON"
+	errUnmarshalCredentials = "cannot unmarshal zonehero credentials as JSON"
+
+	api_key = "test"
 )
 
 // TerraformSetupBuilder builds Terraform a terraform.SetupFn function which
@@ -63,6 +65,10 @@ func TerraformSetupBuilder(version, providerSource, providerVersion string) terr
 		}
 
 		// Set credentials in Terraform provider configuration.
+		ps.Configuration = map[string]any{}
+		if v, ok := creds[api_key]; ok {
+			ps.Configuration[api_key] = v
+		}
 		/*ps.Configuration = map[string]any{
 			"username": creds["username"],
 			"password": creds["password"],
