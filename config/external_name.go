8cb247065f970aafcffbe6675df8ee9fb4eec5b2
Alex Sunins <alex@footprintit.net>
2025-09-05 08:38:27 +0100

    typo

--- config/external_name.go
+++ config/external_name.go
@@ -10,7 +10,7 @@ import "github.com/crossplane/upjet/pkg/config"
 // provider.
 var ExternalNameConfigs = map[string]config.ExternalName{
 	// Import requires using a randomly generated ID from provider: nl-2e21sda
-	"hlb_load_balancer": config.IdentifierFromProvider,
+	"hlb_listener_attachment": config.IdentifierFromProvider,
 }
 
 // ExternalNameConfigurations applies all external name configs listed in the
